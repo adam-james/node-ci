3c47e97199baa2c64a181ea290290a8611a11fc6
Adam Sulewski <adam@pivotalcare.com>
2017-06-30 12:18:25 -0700

    swap mocha for ava

--- test.js
+++ test.js
@@ -1,17 +1,15 @@
 'use strict';
-const test = require('ava');
+const assert = require('assert');
 const request = require('request');
 
 require(__dirname + '/server.js');
 
-test('true', t => {
-  t.pass();
-});
-
-test.cb('get "/"', t => {
-  request('http://localhost:3000', (error, response, body) => {
-    const expected = JSON.stringify({ message: 'Hello, world!' });
-    t.is(body, expected);
-    t.end();
+describe('tests', () => {
+  it('gets "/"', (done) => {
+    request('http://localhost:3000', (error, response, body) => {
+      const expected = JSON.stringify({ message: 'Hello, world!' });
+      assert.equal(body, expected);
+      done();
+    });
   });
 });
