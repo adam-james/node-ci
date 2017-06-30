aead0f56f525903e08eb1ea4c06ed8401647671a
Adam Sulewski <adam@pivotalcare.com>
2017-06-30 13:57:04 -0700

    add ava again

--- test.js
+++ test.js
@@ -1,15 +1,17 @@
 'use strict';
-const assert = require('assert');
+const test = require('ava');
 const request = require('request');
 
 require(__dirname + '/server.js');
 
-describe('tests', () => {
-  it('gets "/"', (done) => {
-    request('http://localhost:3000', (error, response, body) => {
-      const expected = JSON.stringify({ message: 'Hello, world!' });
-      assert.equal(body, expected);
-      done();
-    });
+test('true', t => {
+  t.pass();
+});
+
+test.cb('get "/"', t => {
+  request('http://localhost:3000', (error, response, body) => {
+    const expected = JSON.stringify({ message: 'Hello, world!' });
+    t.is(body, expected);
+    t.end();
   });
 });
